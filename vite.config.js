287a16558150f50219cd737c7e0332df65b30bee
OmarAhmed <omarahmedgodabmox@gmail.com>
2026-09-06 04:39:23 +0300

    Clean up comment in vite.config.js
    Removed comment about changing base path to relative.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/audiophile-website/', // أو تخليه './' علشان يكون مسار نسبي
+  base: '/audiophile-website/', 
 })
